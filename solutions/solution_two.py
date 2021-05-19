2eec883aebff72947e76c901a1b981636fbc152f
RithikaAkula <rithikaakula108@gmail.com>
2021-05-19 08:31:06 +0530

    Added solution_two file

--- solutions/solution_two.py
+++ solutions/solution_two.py
@@ -1,21 +1,11 @@
-def perform_compute_2(x, y, a, b):
-    
-    result = pow(x/y,a+b)
+def nth_term(value) :
+    nth = 0
+ 
+    if (value % 2 == 1) :
+        nth = (value * value) + 1
+    else :
+        nth = (value * value) - 1
+ 
+    return nth
 
-    return result
-
-
-# print("The given expression is reduced to '(x / y) ^ (a + b)'")
-# input_one = int(input("Enter value of x: "))
-# input_two = int(input("Enter value of y: "))
-# input_three = int(input("Enter value of a: "))
-# input_four = int(input("Enter value of b: "))
-
-# if isinstance(input_one,int) and isinstance(input_two,int) and isinstance(input_three,int) and isinstance(input_four,int):
-    
-#     answer = perform_compute_2(input_one, input_two, input_three, input_four)
-# else:
-    
-#     answer="Enter valid integers."
-
-# print(answer)
+# print(nth_term(9))
